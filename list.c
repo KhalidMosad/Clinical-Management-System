3b4ae734f6b2a81de7a8be9f82457d2a47eb17dc
Khalid <khaled.mosad.11@gmail.com>
2021-06-12 21:00:18 +0200

    Description Header file

--- list.c
+++ list.c
@@ -19,63 +19,27 @@
 */
 #include"list.h"
 
-/*****************************************************************
- * Function: CreateList.
- * Description: 
- *      This function takes a list variable that you want to create. 
- * Parameters: 
- *      List *pl------- pointer of type List. 
- * Return: 
- *        void ----- it returns nothing. 
- *               
-*/
+
 void CreateList(List *pl)
 {
     pl->head = NULL;
     pl->size = 0;
 }
 
-/*****************************************************************
- * Function: ListEmpty.
- * Description: 
- *      This function Checks if the Created List is Empty . 
- * Parameters: 
- *      List *pl------- pointer of type List. 
- * Return: 
- *        It returns 0 if the list is empty and 1 if size incremented. 
- *               
-*/
+
 
 
 int ListEmpty(List *pl)
 {
     return (pl->size==0) ;
 }
-/*****************************************************************
- * Function: ListFull.
- * Description: 
- *      This function Checks if the Created List is  Full. 
- * Parameters: 
- *      List *pl------- pointer of type List. 
- * Return: 
- *        It returns 0 because The list is always Empty...... There is no limit to Items in Linkedbased Lists . 
- *               
-*/
+
 int ListFull(List *pl)
 {
     return 0 ;
 
 }
-/*****************************************************************
- * Function: ListSize.
- * Description: 
- *      This function give us information about the size or number of element in the list. 
- * Parameters: 
- *      List *pl------- pointer of type List. 
- * Return: 
- *        It returns size 
- *               
-*/
+
 
 int ListSize(List *pl)
 {
@@ -83,16 +47,7 @@ int ListSize(List *pl)
 
 }
 
-/*****************************************************************
- * Function: DestroyList.
- * Description: 
- *      This function Delete the entire List. 
- * Parameters: 
- *      List *pl------- pointer of type List. 
- * Return: 
- *        It returns nothing 
- *               
-*/
+
 
 void DestroyList(List *pl)
 {
@@ -106,19 +61,7 @@ void DestroyList(List *pl)
    pl->size = 0;
 }
 
-/*****************************************************************
- * Function: InsertList.
- * Description: 
- *      This function insert element to List. 
- * Parameters: 
- *      int pos ------- The specific position to add the listEntry in.
- *      ListEntry e---- This is the item to be inserted.
- *      List *pl------- pointer of type List. 
- * 
- * Return: 
- *        It returns 1 if insertion done successfully and 0 if no. 
- *               
-*/
+
 
 int InsertList(int pos, ListEntry e, List *pl)
 {
@@ -152,19 +95,7 @@ int InsertList(int pos, ListEntry e, List *pl)
         return 0;
 }
 
-/*****************************************************************
- * Function: DeleteList.
- * Description: 
- *      This function Delete just one node from the List. 
- * Parameters: 
- *      int pos ------- The specific position to delete.
- *      ListEntry*pe---- pointer to Hold The data of the deleted node.
- *      List *pl------- pointer of type List. 
- * 
- * Return: 
- *        It returns nothing. 
- *               
-*/
+
 
 
 void DeleteList(int pos, ListEntry *pe, List*pl)
@@ -197,18 +128,7 @@ void DeleteList(int pos, ListEntry *pe, List*pl)
     }
     pl->size--;
 }
-/*****************************************************************
- * Function: TraverseList.
- * Description: 
- *      This function pass through each node of the List and do the required function. 
- * Parameters: 
- *      List *pl------- pointer of type List. 
- *      void(*Visit)(ListEntry) ---- pointer to function
- * 
- * Return: 
- *        It returns nothing. 
- *               
-*/
+
 
 void TraverseList(List *pl, void(*Visit)(ListEntry))
 {
@@ -221,19 +141,7 @@ void TraverseList(List *pl, void(*Visit)(ListEntry))
 
 }
 
-/*****************************************************************
- * Function: RetrieveList.
- * Description: 
- *      This function just to get the data from one node from the List without doing any operation. 
- * Parameters: 
- *      int pos ------- The specific position to get the data from.
- *      ListEntry*pe---- pointer to a variable that Hold The data.
- *      List *pl------- pointer of type List. 
- * 
- * Return: 
- *        It returns nothing. 
- *               
-*/
+
 void RetrieveList(int pos, ListEntry *pe, List *pl)
 {
     // هنا انت عاوز فقط القيمه الي فالمكان دا من غير ما تغيرة ولا تشيله 
@@ -245,19 +153,7 @@ void RetrieveList(int pos, ListEntry *pe, List *pl)
     }
     *pe= q->entry;
 }
-/*****************************************************************
- * Function: ReplaceList.
- * Description: 
- *      This function just to Replace the data of one node from the List with a new data. 
- * Parameters: 
- *      int pos ------- The specific position to Replace the data from.
- *      ListEntry p---- Variable that Hold The new data.
- *      List *pl------- pointer of type List. 
- * 
- * Return: 
- *        It returns nothing. 
- *               
-*/
+
 
 void ReplaceList(int pos, ListEntry e, List *pl)
 {
--- list.h
+++ list.h
@@ -26,17 +26,135 @@ typedef struct list
     int size;
 }List;
 
-
+/*****************************************************************
+ * Function: CreateList.
+ * Description: 
+ *      This function takes a list variable that you want to create. 
+ * Parameters: 
+ *      List *pl------- pointer of type List. 
+ * Return: 
+ *        void ----- it returns nothing. 
+ *               
+*/
 
 void CreateList(List *pl);
+
+/*****************************************************************
+ * Function: ListEmpty.
+ * Description: 
+ *      This function Checks if the Created List is Empty . 
+ * Parameters: 
+ *      List *pl------- pointer of type List. 
+ * Return: 
+ *        It returns 0 if the list is empty and 1 if size incremented. 
+ *               
+*/
 int ListEmpty(List *pl);
+
+/*****************************************************************
+ * Function: ListFull.
+ * Description: 
+ *      This function Checks if the Created List is  Full. 
+ * Parameters: 
+ *      List *pl------- pointer of type List. 
+ * Return: 
+ *        It returns 0 because The list is always Empty...... There is no limit to Items in Linkedbased Lists . 
+ *               
+*/
 int ListFull(List *pl);
+
+/*****************************************************************
+ * Function: ListSize.
+ * Description: 
+ *      This function give us information about the size or number of element in the list. 
+ * Parameters: 
+ *      List *pl------- pointer of type List. 
+ * Return: 
+ *        It returns size 
+ *               
+*/
 int ListSize(List *pl);
+/*****************************************************************
+ * Function: DestroyList.
+ * Description: 
+ *      This function Delete the entire List. 
+ * Parameters: 
+ *      List *pl------- pointer of type List. 
+ * Return: 
+ *        It returns nothing 
+ *               
+*/
 void DestroyList(List *pl);
+
+/*****************************************************************
+ * Function: InsertList.
+ * Description: 
+ *      This function insert element to List. 
+ * Parameters: 
+ *      int pos ------- The specific position to add the listEntry in.
+ *      ListEntry e---- This is the item to be inserted.
+ *      List *pl------- pointer of type List. 
+ * 
+ * Return: 
+ *        It returns 1 if insertion done successfully and 0 if no. 
+ *               
+*/
 int InsertList(int pos, ListEntry e, List *pl);
+
+/*****************************************************************
+ * Function: DeleteList.
+ * Description: 
+ *      This function Delete just one node from the List. 
+ * Parameters: 
+ *      int pos ------- The specific position to delete.
+ *      ListEntry*pe---- pointer to Hold The data of the deleted node.
+ *      List *pl------- pointer of type List. 
+ * 
+ * Return: 
+ *        It returns nothing. 
+ *               
+*/
 void DeleteList(int pos, ListEntry *pe, List*pl);
+/*****************************************************************
+ * Function: TraverseList.
+ * Description: 
+ *      This function pass through each node of the List and do the required function. 
+ * Parameters: 
+ *      List *pl------- pointer of type List. 
+ *      void(*Visit)(ListEntry) ---- pointer to function
+ * 
+ * Return: 
+ *        It returns nothing. 
+ *               
+*/
 void TraverseList(List *pl, void(*Visit)(ListEntry));
+/*****************************************************************
+ * Function: RetrieveList.
+ * Description: 
+ *      This function just to get the data from one node from the List without doing any operation. 
+ * Parameters: 
+ *      int pos ------- The specific position to get the data from.
+ *      ListEntry*pe---- pointer to a variable that Hold The data.
+ *      List *pl------- pointer of type List. 
+ * 
+ * Return: 
+ *        It returns nothing. 
+ *               
+*/
 void RetrieveList(int pos, ListEntry *pe, List *pl);
+/*****************************************************************
+ * Function: ReplaceList.
+ * Description: 
+ *      This function just to Replace the data of one node from the List with a new data. 
+ * Parameters: 
+ *      int pos ------- The specific position to Replace the data from.
+ *      ListEntry p---- Variable that Hold The new data.
+ *      List *pl------- pointer of type List. 
+ * 
+ * Return: 
+ *        It returns nothing. 
+ *               
+*/
 void ReplaceList(int pos, ListEntry e, List *pl);
 
 
